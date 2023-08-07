c7b1fd2318f0e11bc737c1c656c53998955b82e6
TenzinDala <tenzindala2000@gmail.com>
2023-08-07 06:07:14 -0400

    Added second form and third form. Need to add receipt now.

--- PRG-Project/Forms/Form1.cs
+++ PRG-Project/Forms/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PRG_Project.Forms;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,6 +15,11 @@ namespace PRG_Project
 {
     public partial class MainPage : Form
     {
+        public static string SelectedItemID;
+        public static string price;
+        public static string productQuantity;
+        public static string Sold;
+
         public MainPage()
         {
             InitializeComponent();
@@ -25,7 +31,7 @@ namespace PRG_Project
             string constr = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=PRG-Project.accdb";
             //string constr = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=PRG-Project.accdb";
 
-            string sqlstr = "SELECT TOP 5 * FROM Products";
+            string sqlstr = "SELECT * FROM Products ORDER BY ComputerID ASC";
 
             OleDbDataAdapter da = new OleDbDataAdapter(sqlstr, constr);
             da.Fill(vt);
@@ -51,7 +57,14 @@ namespace PRG_Project
                 OleDbDataAdapter da = new OleDbDataAdapter(sqlstr, constr);
                 da.Fill(vt);
                 da.Dispose();
-                dataGridProducts.DataSource = vt;
+                if (vt.Rows.Count == 0)
+                {
+                    MessageBox.Show("No Products Found!!!");
+                }
+                else
+                {
+                    dataGridProducts.DataSource = vt;
+                }
             }
             else
             {
@@ -64,10 +77,63 @@ namespace PRG_Project
                 OleDbDataAdapter da = new OleDbDataAdapter(sqlstr, constr);
                 da.Fill(vt);
                 da.Dispose();
-                dataGridProducts.DataSource = vt;
-
+                if (vt.Rows.Count == 0)
+                {
+                    MessageBox.Show("No Products Found!!!");
+                }
+                else
+                {
+                    dataGridProducts.DataSource = vt;
+                }
             }
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(dataGridProducts.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridProducts.SelectedRows[0];
+
+                SelectedItemID = selectedRow.Cells[0].Value.ToString();
+                productQuantity = selectedRow.Cells[3].Value.ToString();
+                price = selectedRow.Cells[4].Value.ToString();
+                Sold = selectedRow.Cells[5].Value.ToString();
+
+
+                Information information = new Information();
+                information.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Please select a row!!!");
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnSearch_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void btnSearch_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
         }
     }
 }
---
+++ PRG-Project/Forms/Order.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRG_Project.Forms
+{
+    public partial class Order : Form
+    {
+        public Order()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            MainPage mainPage = new MainPage();
+            mainPage.Show();
+            this.Hide();
+        }
+
+        private void buttonFinish_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //To Detect if quantity is left and also to update quantity in Products Table
+                int quantity = Convert.ToInt32(textBoxQuantity.Text);
+                int quantityNew = Int32.Parse(MainPage.productQuantity) - quantity;
+                int SoldNew = Int32.Parse(MainPage.Sold) + quantity;
+
+                string constr = "Provider=Microsoft.ACE.OLEDB.12.0;" + "Data Source=PRG-Project.accdb";
+
+                //string sqlstrUpdateCustomerTable = "UPDATE Products SET Quantity= 0 WHERE ComputerID= 1";
+                string sqlstrUpdateCustomerTable = "UPDATE Products SET Quantity = "+quantityNew+", Sold = "+SoldNew+" WHERE ComputerID = "+Int32.Parse(MainPage.SelectedItemID);
+
+
+                OleDbDataAdapter daUpdateCustomerTable = new OleDbDataAdapter();
+
+                OleDbConnection conUpdateCustomerTable = new OleDbConnection(constr);
+                conUpdateCustomerTable.Open();
+
+                //string sqlstrUpdateCustomerTable = "UPDATE Products SET Quantity = "+quantityNew+", Sold = "+quantity+" WHERE CustomerID = "+Int32.Parse(MainPage.SelectedItemID);
+                //string sqlstrUpdateCustomerTable = "UPDATE Products SET Quantity="+quantityNew+" WHERE CustomerID='"+MainPage.SelectedItemID+"'";
+
+
+                daUpdateCustomerTable.UpdateCommand = new OleDbCommand(sqlstrUpdateCustomerTable, conUpdateCustomerTable);
+                daUpdateCustomerTable.UpdateCommand.ExecuteNonQuery();
+                conUpdateCustomerTable.Close();
+
+
+
+
+
+
+
+                //For Inserting into the Customers Table
+                OleDbDataAdapter daCustomerTable = new OleDbDataAdapter();
+
+                OleDbConnection con = new OleDbConnection(constr);
+                con.Open();
+
+                string sqlstr = "INSERT INTO Customers(FirstName,LastName,CreditCard)" + "VALUES('" + textBoxFirstName.Text + "','" + textBoxLastName.Text + "','" + textBoxCardNumber.Text + "')";
+
+                daCustomerTable.InsertCommand = new OleDbCommand(sqlstr, con);
+                daCustomerTable.InsertCommand.ExecuteNonQuery();
+                con.Close();
+
+                //Retrieving Customer ID to place in Orders Table
+
+                DataTable vt = new DataTable();
+
+                string sqlstrSelectCustomerTable = "SELECT * FROM Customers WHERE FirstName LIKE '" + textBoxFirstName.Text+ "' AND LastName LIKE '"+textBoxLastName.Text+"'";
+
+                OleDbDataAdapter daOrderTable = new OleDbDataAdapter(sqlstrSelectCustomerTable, constr);
+                daOrderTable.Fill(vt);
+                daOrderTable.Dispose();
+                int customerID = Convert.ToInt32(vt.Rows[0]["CustomerID"]);
+
+                //Updating in Orders Table with CustomerID, ProductID and Quantities
+
+                string sqlstrUpdateOrdersTable = "INSERT INTO Orders(CustomerID,ProductID,Quantities)" + "VALUES("+customerID+",'" +MainPage.SelectedItemID+ "',"+quantity+")";
+                OleDbConnection conOrdersTable = new OleDbConnection(constr);
+                OleDbDataAdapter daUpdateOrderTable = new OleDbDataAdapter();
+                conOrdersTable.Open();
+                daUpdateOrderTable.InsertCommand = new OleDbCommand(sqlstrUpdateOrdersTable, conOrdersTable);
+                daUpdateOrderTable.InsertCommand.ExecuteNonQuery();
+                conOrdersTable.Close();
+
+
+                MessageBox.Show("Checked Out Succesfully!!!");
+                Reciept reciept = new Reciept();
+                reciept.Show();
+                this.Hide();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
